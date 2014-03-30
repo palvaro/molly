0f8c68f6ee50a261e70b1950cabfa4c095e6fbe8
Josh Rosen <rosenville@gmail.com>
2014-03-29 17:15:25 -0700

    Force space-time graph timelines into straight lines.

--- src/main/scala/edu/berkeley/cs/boom/molly/report/SpacetimeDiagramGenerator.scala
+++ src/main/scala/edu/berkeley/cs/boom/molly/report/SpacetimeDiagramGenerator.scala
@@ -7,20 +7,20 @@ import edu.berkeley.cs.boom.molly.derivations.Message
 object SpacetimeDiagramGenerator extends GraphvizPrettyPrinter {
   def generate(failureSpec: FailureSpec, messages: Seq[Message]): String = {
     val processNodes = failureSpec.nodes.map { n =>
-      node(s"proc_$n", "label" -> s"Process $n")
+      node(s"proc_$n", "label" -> s"Process $n", "group" -> n)
     }
 
     val nodeTimes = for (n <- failureSpec.nodes; t <- 0 to failureSpec.eot) yield {
       val crashed = !failureSpec.crashes.filter(c => c.node == n && c.time <= t).isEmpty
       val styles =
         if (crashed) Seq("color" -> "red", "fontsize" -> "10", "label" -> "CRASHED")
-        else  Seq("label" -> s"$t")
+        else Seq("label" -> s"$t", "group" -> n)
       node(s"node_${n}_$t", styles: _*)
     }
 
-    val nodeTimeEdges = failureSpec.nodes.flatMap { n =>
-      val es = (0 to failureSpec.eot - 1).map { t => diEdge(s"node_${n}_$t", s"node_${n}_${t + 1}") }
-      es :+ diEdge(s"proc_$n", s"node_${n}_0", "weight" -> "2")
+    val timelineLines = failureSpec.nodes.map { n =>
+      val nodes = (0 to failureSpec.eot).map { t => s"node_${n}_$t" }.toSeq
+      "edge[weight=2];" <@@> s"proc_$n -> " <> nodes.mkString(" -> ") <> semi
     }
 
     val messageEdges = messages.map { case Message(table, from, to, sendTime, receiveTime) =>
@@ -38,7 +38,9 @@ object SpacetimeDiagramGenerator extends GraphvizPrettyPrinter {
       linebreak <>
       "rankdir=LR" <@@>
       subgraph("cluster_proc_nodes", "", processNodes) <@@>
-      (nodeTimes ++ nodeTimeEdges ++ messageEdges).reduce(_ <@@> _)
+      nodeTimes.reduce(_ <@@> _) <@@>
+      timelineLines.reduce(_ <@@> _) <@@>
+      messageEdges.foldLeft(empty)(_ <@@> _)
     ) <> linebreak)
     super.pretty(dot)
   }
