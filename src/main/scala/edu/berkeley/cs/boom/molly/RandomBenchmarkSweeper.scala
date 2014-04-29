2fbf89f594d8d73645e73177de405c5cd9eb0880
Josh Rosen <rosenville@gmail.com>
2014-04-29 16:19:23 -0700

    Add changes that I forgot from last commit.

--- src/main/scala/edu/berkeley/cs/boom/molly/RandomBenchmarkSweeper.scala
+++ src/main/scala/edu/berkeley/cs/boom/molly/RandomBenchmarkSweeper.scala
@@ -1,48 +1,32 @@
 package edu.berkeley.cs.boom.molly
 
-import java.io.File
-import com.github.tototoshi.csv.CSVWriter
-import edu.berkeley.cs.boom.molly.wrappers.{C4, APR}
-import jnr.ffi.{Pointer, LibraryLoader}
-import jnr.ffi.byref.PointerByReference
-
+import scala.sys.process._
 
 /**
  * Benchmarks the randomized search strategy against an entire corpus of programs.
  */
 object RandomBenchmarkSweeper {
 
-  val OUTPUT_DIR = "random_benchmarks"
   val NUM_RUNS = 100
 
   val corpus = Seq(
     // ("Input programs", "eot", "eff", "nodes", "crashes")
     (Seq("simplog.ded", "deliv_assert.ded"),  4,   2, Seq("a", "b", "c"), 0),
-    (Seq("rdlog.ded", "deliv_assert.ded"),     4,  2, Seq("a", "b", "c"), 1)
-    //(Seq("replog.ded", "deliv_assert.ded"),     8, 6, Seq("a", "b", "c"), 1),
-    //(Seq("classic_rb.ded", "deliv_assert.ded"), 5, 3, Seq("a", "b", "c"), 0),
-    //(Seq("2pc.ded", "2pc_assert.ded"),          5, 0, Seq("a", "b", "C", "d"), 1),
-    //(Seq("2pc_ctp.ded", "2pc_assert.ded"),      6, 0, Seq("a", "b", "C", "d"), 1),
-    //(Seq("3pc.ded", "2pc_assert.ded"),          9, 7, Seq("a", "b", "C", "d"), 1),
-    //(Seq("kafka.ded"),                          6, 4, Seq("a", "b", "c", "C", "Z"), 1)
+    (Seq("rdlog.ded", "deliv_assert.ded"),     4,  2, Seq("a", "b", "c"), 1),
+    (Seq("replog.ded", "deliv_assert.ded"),     8, 6, Seq("a", "b", "c"), 1),
+    (Seq("classic_rb.ded", "deliv_assert.ded"), 5, 3, Seq("a", "b", "c"), 0),
+    (Seq("2pc.ded", "2pc_assert.ded"),          5, 0, Seq("a", "b", "C", "d"), 1),
+    (Seq("2pc_ctp.ded", "2pc_assert.ded"),      6, 0, Seq("a", "b", "C", "d"), 1),
+    (Seq("3pc.ded", "2pc_assert.ded"),          9, 7, Seq("a", "b", "C", "d"), 1),
+    (Seq("kafka.ded"),                          6, 4, Seq("a", "b", "c", "C", "Z"), 1)
   )
 
   def main(args: Array[String]) {
-    val outputDirectory = new File(OUTPUT_DIR)
-    outputDirectory.mkdir()
-
     corpus.par.foreach { case (inputPrograms, eot, eff, nodes, crashes) =>
-      val inputFiles = inputPrograms.map(name => new File("../examples_ft/" + name))
-      val config = Config(eot, eff, crashes, nodes, inputFiles, strategy = "random")
-      val csvFile = new File(outputDirectory, s"${inputPrograms.head}_t_${eot}_f_${eff}_c_${crashes}.csv")
-      val csvWriter = CSVWriter.open(csvFile)
-      try {
-        (1 to NUM_RUNS).foreach { _ =>
-          csvWriter.writeRow(RandomBenchmark.findFirstCounterexample(config).productIterator.toSeq)
-        }
-      } finally {
-        csvWriter.close()
-      }
+      val inputFiles = inputPrograms.map(name => "../examples_ft/" + name)
+      val command = s"-N ${nodes.mkString(",")} -t $eot -f $eff -c $crashes --max-runs $NUM_RUNS ${inputFiles.mkString(" ")}"
+      println(s"Running command '$command'")
+      Seq("sbt", s"run-main edu.berkeley.cs.boom.molly.RandomBenchmark $command").!
     }
   }
 }
