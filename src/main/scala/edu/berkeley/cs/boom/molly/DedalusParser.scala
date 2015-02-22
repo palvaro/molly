d96315d94b9563ff9848684cf8ec23add5a9fb81
Josh Rosen <rosenville@gmail.com>
2015-02-22 14:32:11 -0800

    Combine DedalusParser object and trait; add a few comments.
    There wasn’t an obvious reason for this to be its own trait.

--- src/main/scala/edu/berkeley/cs/boom/molly/DedalusParser.scala
+++ src/main/scala/edu/berkeley/cs/boom/molly/DedalusParser.scala
@@ -1,13 +1,34 @@
 package edu.berkeley.cs.boom.molly
 
-import org.kiama.util.PositionedParserUtilities
-import edu.berkeley.cs.boom.molly.ast._
 import java.io.File
+
 import scala.io.Source
 
+import org.kiama.util.PositionedParserUtilities
 
-trait DedalusParser extends PositionedParserUtilities {
+import edu.berkeley.cs.boom.molly.ast._
 
+/**
+ * Parser for the Dedalus language, which is syntactically similar to Datalog / Prolog.
+ * Refer to the `molly.ast` package for the definitions of the types returned by this parser.
+ *
+ * For more information on Dedalus, see the "Dedalus: Datalog in Space and Time" tech report:
+ * http://www.eecs.berkeley.edu/Pubs/TechRpts/2009/EECS-2009-173.html
+ *
+ * This parser is implemented using Kiama parser combinators; for more details, see
+ * https://code.google.com/p/kiama/wiki/Parsing
+ */
+object DedalusParser extends PositionedParserUtilities {
+
+  def parseProgram(str: CharSequence): Program = {
+    parseAll(program, str).get
+  }
+
+  def parseProgramAndIncludes(includeSearchPath: File)(str: CharSequence): Program = {
+    processIncludes(parseProgram(str), includeSearchPath)
+  }
+
+  // Define a bunch of constants / building blocks that will be used by later rules
   lazy val ident = "[a-zA-Z0-9._?@]+".r
   lazy val semi = ";"
   lazy val number = "[0-9]+".r ^^ { s => s.toInt}
@@ -19,13 +40,18 @@ trait DedalusParser extends PositionedParserUtilities {
     '@' ~> number ^^ Tick
   lazy val op = "==" | "!=" | "+" | "-" | "/" | "*" | "<" | ">" | "<=" | ">="
 
-  lazy val constant: Parser[Constant] = string ^^ StringLiteral | number ^^ IntLiteral | ident ^^ Identifier
+  // Define the language of expressions that can appear in rule bodies
+  lazy val constant: Parser[Constant] =
+    string ^^ StringLiteral |
+    number ^^ IntLiteral |
+    ident ^^ Identifier
   lazy val expr: Parser[Expr] = constant ~ op ~ exprOrConstant ^^ { case c ~ o ~ e => Expr(c, o, e)}
   lazy val exprOrConstant: Parser[Expression] = expr | constant
   lazy val aggregate = ident ~ "<" ~ ident ~ ">" ^^ {
     case aggName ~ "<" ~ aggCol ~ ">" => Aggregate(aggName, aggCol)
   }
 
+  // A program is a collection of rules and facts
   lazy val program: Parser[Program] = rep(clause) ^^ { clauses =>
     Program(clauses.collect { case r: Rule => r },
       clauses.collect { case p: Predicate => p },
@@ -51,16 +77,6 @@ trait DedalusParser extends PositionedParserUtilities {
 
   // See https://stackoverflow.com/questions/5952720
   override val whiteSpace = """(\s|//.*|(?m)/\*(\*(?!/)|[^*])*\*/)+""".r
-}
-
-object DedalusParser extends DedalusParser {
-  def parseProgram(str: CharSequence): Program = {
-    parseAll(program, str).get
-  }
-
-  def parseProgramAndIncludes(includeSearchPath: File)(str: CharSequence): Program = {
-    processIncludes(parseProgram(str), includeSearchPath)
-  }
 
   private def processIncludes(program: Program, includeSearchPath: File): Program = {
     val includes = program.includes.map { include =>
