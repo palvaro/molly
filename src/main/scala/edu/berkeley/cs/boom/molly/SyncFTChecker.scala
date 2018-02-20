c3881ae7ef6b65a63c9f083771b599a311d5f42c
Lennart Oldenburg <hello@lennartoldenburg.de>
2018-02-19 21:57:41 -0800

    Fixed bug where supplying the '--negative-support' flag would not disable negative support.

--- src/main/scala/edu/berkeley/cs/boom/molly/SyncFTChecker.scala
+++ src/main/scala/edu/berkeley/cs/boom/molly/SyncFTChecker.scala
@@ -7,12 +7,11 @@ import com.typesafe.scalalogging.LazyLogging
 import edu.berkeley.cs.boom.molly.DedalusRewrites._
 import edu.berkeley.cs.boom.molly.DedalusParser._
 import edu.berkeley.cs.boom.molly.report.HTMLWriter
-import com.codahale.metrics.{Slf4jReporter, MetricRegistry}
+import com.codahale.metrics.{ Slf4jReporter, MetricRegistry }
 import java.util.concurrent.TimeUnit
 import scalaz.syntax.id._
 import scalaz.EphemeralStream
-import edu.berkeley.cs.boom.molly.derivations.{SAT4JSolver, Z3Solver}
-
+import edu.berkeley.cs.boom.molly.derivations.{ SAT4JSolver, Z3Solver }
 
 case class Config(
   eot: Int = 3,
@@ -28,30 +27,31 @@ case class Config(
   disableDotRendering: Boolean = false,
   findAllCounterexamples: Boolean = false,
   negativeSupport: Boolean = true,
-  maxRuns: Int = Int.MaxValue
-)
+  maxRuns: Int = Int.MaxValue)
 
 object SyncFTChecker extends LazyLogging {
+
   val parser = new scopt.OptionParser[Config]("syncftChecker") {
     head("syncftchecker", "0.1")
-    opt[Int]('t', "EOT") text "end of time (default 3)" action { (x, c) => c.copy(eot = x)}
-    opt[Int]('f', "EFF") text "end of finite failures (default 2)" action { (x, c) => c.copy(eff = x)}
-    opt[Int]('c', "crashes") text "crash failures (default 0)" action { (x, c) => c.copy(crashes = x)}
-    opt[String]('N', "nodes") text "a comma-separated list of nodes (required)" required() action { (x, c) => c.copy(nodes = x.split(','))}
-    opt[String]("solver") text "the solver to use ('z3' or 'sat4j' or 'ilp')" action { (x, c) => c.copy(solver = x)} validate { x => if (x != "z3" && x != "sat4j" && x != "ilp") failure("solver should be 'z3' or 'sat4j' or 'ilp'") else success }
-    opt[String]("strategy") text "the search strategy ('sat', 'random' or 'pcausal')" action { (x, c) => c.copy(strategy = x)} validate { x => if (x != "sat" && x != "random" && x != "pcausal") failure("strategy should be 'sat', 'random', or 'pcausal'") else success }
-    opt[Unit]("use-symmetry") text "use symmetry to skip equivalent failure scenarios" action { (x, c) => c.copy(useSymmetry = true) }
-    opt[Unit]("prov-diagrams") text "generate provenance diagrams for each execution" action { (x, c) => c.copy(generateProvenanceDiagrams = true) }
-    opt[Unit]("disable-dot-rendering") text "disable automatic rendering of `dot` diagrams" action { (x, c) => c.copy(disableDotRendering = true) }
-    opt[Unit]("find-all-counterexamples") text "continue after finding the first counterexample" action { (x, c) => c.copy(findAllCounterexamples = true) }
-    opt[Unit]("negative-support") text "Negative support.  Slow, but necessary for completeness" action {(x, c) => c.copy(negativeSupport =  true) }
-    arg[File]("<file>...") unbounded() minOccurs 1 text "Dedalus files" action { (x, c) => c.copy(inputPrograms = c.inputPrograms :+ x)}
+    opt[Int]('t', "EOT") text "end of time (default 3)" action { (x, c) => c.copy(eot = x) }
+    opt[Int]('f', "EFF") text "end of finite failures (default 2)" action { (x, c) => c.copy(eff = x) }
+    opt[Int]('c', "crashes") text "crash failures (default 0)" action { (x, c) => c.copy(crashes = x) }
+    opt[String]('N', "nodes") text "a comma-separated list of nodes (required)" required () action { (x, c) => c.copy(nodes = x.split(',')) }
+    opt[String]("solver") text "the solver to use ('z3' or 'sat4j' or 'ilp')" action { (x, c) => c.copy(solver = x) } validate { x => if (x != "z3" && x != "sat4j" && x != "ilp") failure("solver should be 'z3' or 'sat4j' or 'ilp'") else success }
+    opt[String]("strategy") text "the search strategy ('sat', 'random' or 'pcausal')" action { (x, c) => c.copy(strategy = x) } validate { x => if (x != "sat" && x != "random" && x != "pcausal") failure("strategy should be 'sat', 'random', or 'pcausal'") else success }
+    opt[Unit]("use-symmetry") text "use symmetry to skip equivalent failure scenarios" action { (_, c) => c.copy(useSymmetry = true) }
+    opt[Unit]("prov-diagrams") text "generate provenance diagrams for each execution" action { (_, c) => c.copy(generateProvenanceDiagrams = true) }
+    opt[Unit]("disable-dot-rendering") text "disable automatic rendering of `dot` diagrams" action { (_, c) => c.copy(disableDotRendering = true) }
+    opt[Unit]("find-all-counterexamples") text "continue after finding the first counterexample" action { (_, c) => c.copy(findAllCounterexamples = true) }
+    opt[Unit]("negative-support") text "Disable negative support. Mind, that negative support is slow, but necessary for completeness." action { (_, c) => c.copy(negativeSupport = false) }
+    arg[File]("<file>...") unbounded () minOccurs 1 text "Dedalus files" action { (x, c) => c.copy(inputPrograms = c.inputPrograms :+ x) }
   }
 
   /**
    * Like `takeWhile`, but also takes the first element NOT satisfying the predicate.
    */
   def takeUpTo[T](stream: EphemeralStream[T], pred: T => Boolean): EphemeralStream[T] = {
+
     if (stream.isEmpty) {
       stream
     } else {
@@ -65,25 +65,30 @@ object SyncFTChecker extends LazyLogging {
   }
 
   def check(config: Config, metrics: MetricRegistry): EphemeralStream[Run] = {
+
     val combinedInput = config.inputPrograms.flatMap(Source.fromFile(_).getLines()).mkString("\n")
     val includeSearchPath = config.inputPrograms(0).getParentFile
     val program = combinedInput |> parseProgramAndIncludes(includeSearchPath) |> referenceClockRules |> splitAggregateRules |> addProvenanceRules
     val failureSpec = FailureSpec(config.eot, config.eff, config.crashes, config.nodes.toList)
+
     val solver = config.solver match {
       case "z3" => Z3Solver
       case "sat4j" => SAT4JSolver
       //case "ilp" => ILPSolver
       case s => throw new IllegalArgumentException(s"unknown solver $s")
     }
-    val verifier = new Verifier(failureSpec, program, solver, causalOnly = (config.strategy == "pcausal"),
-      useSymmetry = config.useSymmetry, negativeSupport = config.negativeSupport)(metrics)
+
+    val verifier = new Verifier(failureSpec, program, solver, causalOnly = (config.strategy == "pcausal"), useSymmetry = config.useSymmetry, negativeSupport = config.negativeSupport)(metrics)
+
     logger.info(s"Gross estimate: ${failureSpec.grossEstimate} runs")
+
     val results = config.strategy match {
       case "sat" => verifier.verify
       case "pcausal" => verifier.verify
       case "random" => verifier.random
       case s => throw new IllegalArgumentException(s"unknown strategy $s")
     }
+
     if (config.findAllCounterexamples) {
       results
     } else {
@@ -92,6 +97,7 @@ object SyncFTChecker extends LazyLogging {
   }
 
   def main(args: Array[String]) {
+
     val metrics = new MetricRegistry
     val metricsReporter = Slf4jReporter.forRegistry(metrics)
       .outputTo(logger.underlying)
@@ -100,23 +106,25 @@ object SyncFTChecker extends LazyLogging {
       .build()
 
     parser.parse(args, Config()) map { config =>
+
       // Kind of a hack to tee an ephemeral stream
       var firstCounterExample: FailureSpec = null
-      val results = check(config, metrics).map { case r =>
-        if (firstCounterExample == null && r.status == RunStatus("failure"))
-          firstCounterExample = r.failureSpec
-        r
+      val results = check(config, metrics).map {
+        case r =>
+          if (firstCounterExample == null && r.status == RunStatus("failure"))
+            firstCounterExample = r.failureSpec
+          r
       }
+
       // TODO: name the output directory after the input filename and failure spec.
-      HTMLWriter.write(new File("output"), Nil, results, config.generateProvenanceDiagrams,
-        config.disableDotRendering)
+      HTMLWriter.write(new File("output"), Nil, results, config.generateProvenanceDiagrams, config.disableDotRendering)
       println("-" * 80)
-      metricsReporter.report()  // This appears after the HTML writing due to laziness
+      metricsReporter.report() // This appears after the HTML writing due to laziness
       println("-" * 80)
+
       firstCounterExample match {
         case null => println("No counterexamples found".green)
-        case fs: FailureSpec =>
-          println(s"Found counterexamples; first is:\n${fs.crashes ++ fs.omissions}".red)
+        case fs: FailureSpec => println(s"Found counterexamples; first is:\n${fs.crashes ++ fs.omissions}".red)
       }
     } getOrElse {
       // Error messages
--- src/main/scala/edu/berkeley/cs/boom/molly/Verifier.scala
+++ src/main/scala/edu/berkeley/cs/boom/molly/Verifier.scala
@@ -11,7 +11,7 @@ import scalaz._
 
 import edu.berkeley.cs.boom.molly.ast.Program
 import edu.berkeley.cs.boom.molly.derivations._
-import edu.berkeley.cs.boom.molly.symmetry.{SymmetryChecker, SymmetryAwareSet}
+import edu.berkeley.cs.boom.molly.symmetry.{ SymmetryChecker, SymmetryAwareSet }
 import edu.berkeley.cs.boom.molly.wrappers.C4Wrapper
 
 case class RunStatus(underlying: String) extends AnyVal
@@ -38,25 +38,22 @@ case class Run(
 /**
  * Implements Molly's main forwards-backwards verification loop.
  *
- * @param failureSpec a description of possible failure scenarios that the verifier is allowed
- *                    to consider
+ * @param failureSpec a description of possible failure scenarios that the verifier is allowed to consider
  * @param program the program to verify
  * @param solver the solver to use (either Z3 or SAT4J)
  * @param causalOnly TODO(josh): Document this
  * @param useSymmetry if true, use symmetry analysis to prune the exploration of failure scenarios
  *                    that are isomorphic to ones that we have already considered
- * @param negativeSupport if true, explore beneath negative subgoals when constructing provenance
- *                        trees
+ * @param negativeSupport if true, explore beneath negative subgoals when constructing provenance trees
  * @param metricRegistry a CodaHale metrics registry, for logging performance statistics
  */
 class Verifier(
   failureSpec: FailureSpec,
   program: Program,
   solver: Solver = Z3Solver,
-  causalOnly: Boolean  = false,
+  causalOnly: Boolean = false,
   useSymmetry: Boolean = false,
-  negativeSupport: Boolean = false
-)(implicit val metricRegistry: MetricRegistry) extends LazyLogging with InstrumentedBuilder {
+  negativeSupport: Boolean = false)(implicit val metricRegistry: MetricRegistry) extends LazyLogging with InstrumentedBuilder {
 
   private val failureFreeSpec = failureSpec.copy(eff = 0, maxCrashes = 0)
   private val failureFreeProgram = DedalusTyper.inferTypes(failureFreeSpec.addClockFacts(program))
@@ -91,15 +88,16 @@ class Verifier(
    * Same as `verify`, except uses a naive random exploration of the space of possible failures.
    */
   def random: EphemeralStream[Run] = {
-    val provenanceReader =
-      new ProvenanceReader(failureFreeProgram, failureFreeSpec, failureFreeUltimateModel, negativeSupport)
+
+    val provenanceReader = new ProvenanceReader(failureFreeProgram, failureFreeSpec, failureFreeUltimateModel, negativeSupport)
     val messages = provenanceReader.messages
-    val failureFreeRun =
-      Run(runId.getAndIncrement, RunStatus("success"), failureSpec, failureFreeUltimateModel, messages, Nil)
+    val failureFreeRun = Run(runId.getAndIncrement, RunStatus("success"), failureSpec, failureFreeUltimateModel, messages, Nil)
+
     failureFreeRun ##:: doRandom
   }
 
   private def whichProvenance(reader: ProvenanceReader, orig: List[GoalNode]): List[GoalNode] = {
+
     if (causalOnly) {
       reader.getPhonyDerivationTreesForTable("post")
     } else {
@@ -108,12 +106,15 @@ class Verifier(
   }
 
   private def doRandom: EphemeralStream[Run] = {
+
     val run = someRandomRun
     logger.warn(s"run info is ${run.status}")
+
     run ##:: doRandom
   }
 
   private def someRandomRun: Run = {
+
     val numCrashes = rando.nextInt(originalSpec.maxCrashes + 1)
     val crashes = for (
       crash <- rando.shuffle(originalSpec.nodes).take(numCrashes);
@@ -125,19 +126,19 @@ class Verifier(
     val messageLoss = for (
       from <- originalSpec.nodes;
       to <- originalSpec.nodes;
-      time <- 1 to originalSpec.eff - 1
-      if (rando.nextInt % originalSpec.eff) == 1
-      if from != to
+      time <- 1 to originalSpec.eff - 1 if (rando.nextInt % originalSpec.eff) == 1 if from != to
     ) yield {
       MessageLoss(from, to, time)
     }
 
     logger.warn(s"Testing with crashes $crashes and losses $messageLoss")
+
     val randomSpec = originalSpec.copy(crashes = crashes.toSet, omissions = messageLoss.toSet)
     val failProgram = DedalusTyper.inferTypes(randomSpec.addClockFacts(program))
     val model = new C4Wrapper("with_errors", failProgram).run
     val provenanceReader = new ProvenanceReader(failProgram, randomSpec, model, negativeSupport)
     val messages = provenanceReader.messages
+
     if (isGood(model)) {
       Run(runId.getAndIncrement, RunStatus("success"), randomSpec, model, messages, Nil)
     } else {
@@ -146,22 +147,24 @@ class Verifier(
     }
   }
 
-
   /**
    * Run the verification, returning a lazy ephemeral stream of results.  You can perform
    * `filter` or `dropWhile` operations on this stream in order to run the verifier until
    * it finds a counterexample or terminates.
    */
   def verify: EphemeralStream[Run] = {
+
     logger.warn(s"DO verify")
 
-    val provenanceReader =
-      new ProvenanceReader(failureFreeProgram, failureFreeSpec, failureFreeUltimateModel, negativeSupport)
+    val provenanceReader = new ProvenanceReader(failureFreeProgram, failureFreeSpec, failureFreeUltimateModel, negativeSupport)
+
     logger.debug("get messages")
     val messages = provenanceReader.messages
+
     logger.debug("GET TREES")
     val provenance_orig = provenanceReader.getDerivationTreesForTable("post")
     val provenance = whichProvenance(provenanceReader, provenance_orig)
+
     logger.debug("done TREES")
 
     //provenance.foreach{ p =>
@@ -173,30 +176,36 @@ class Verifier(
 
     val satModels = solver.solve(failureSpec, provenance, messages)
     //println(s"SAT models: $satModels")
-    val failureFreeRun =
-      Run(runId.getAndIncrement, RunStatus("success"), failureSpec, failureFreeUltimateModel, messages, provenance_orig)
+    val failureFreeRun = Run(runId.getAndIncrement, RunStatus("success"), failureSpec, failureFreeUltimateModel, messages, provenance_orig)
+
     failureFreeRun ##:: doVerify(satModels.iterator)
   }
 
   private def doVerify(queueToVerify: Iterator[FailureSpec]): EphemeralStream[Run] = {
+
     val unexplored = queueToVerify.dropWhile(alreadyExplored.contains)
+
     if (unexplored.isEmpty) {
       EphemeralStream.emptyEphemeralStream
     } else {
+
       val failureSpec = unexplored.next()
-      assert (!alreadyExplored.contains(failureSpec))
+      assert(!alreadyExplored.contains(failureSpec))
+
       val (run, potentialCounterexamples) = runFailureSpec(failureSpec)
       alreadyExplored += failureSpec
+
       run ##:: doVerify(queueToVerify ++ potentialCounterexamples)
     }
   }
 
   private def isGood(model: UltimateModel): Boolean = {
+
     // this, OR pre is empty in the current model for each post in failureFreeGood
     val pres = model.tableAtTime("pre", failureSpec.eot).toSet
     val posts = model.tableAtTime("post", failureSpec.eot).toSet
 
-    logger.debug(s"FFG: $failureFreeGood.  PRE: $pres. ")
+    logger.debug(s"FFG: ${failureFreeGood}.  PRE: ${pres}. ")
 
     val diff = failureFreeGood -- model.tableAtTime("post", failureSpec.eot)
 
@@ -211,10 +220,14 @@ class Verifier(
    * counterexamples that should be explored.
    */
   private def runFailureSpec(failureSpec: FailureSpec): (Run, Set[FailureSpec]) = {
+
     logger.info(s"Retesting with crashes ${failureSpec.crashes} and losses ${failureSpec.omissions}")
+
     val failProgram = DedalusTyper.inferTypes(failureSpec.addClockFacts(program))
     val model = new C4Wrapper("with_errors", failProgram).run
+
     logger.info(s"'post' is ${model.tableAtTime("post", failureSpec.eot)}")
+
     val provenanceReader = new ProvenanceReader(failProgram, failureSpec, model, negativeSupport)
     val messages = provenanceReader.messages
     val provenance_orig = provenanceReader.getDerivationTreesForTable("post")
@@ -223,21 +236,24 @@ class Verifier(
     //  val tups = p.allTups
     //  logger.debug("THIS prov, " + tups.toString)
     //}
-
+    //
 
     if (isGood(model)) {
+
       // This run may have used more channels than the original run; verify
       // that omissions on those new channels don't produce counterexamples:
       logger.warn(s"Solving...")
+
       val seed: Set[SolverVariable] = failureSpec.crashes ++ failureSpec.omissions
-      val potentialCounterexamples =
-        solver.solve(failureSpec, provenance, messages, seed).toSet -- Set(failureSpec)
-      val run =
-        Run(runId.getAndIncrement, RunStatus("success"), failureSpec, model, messages, provenance_orig)
+      val potentialCounterexamples = solver.solve(failureSpec, provenance, messages, seed).toSet -- Set(failureSpec)
+      val run = Run(runId.getAndIncrement, RunStatus("success"), failureSpec, model, messages, provenance_orig)
+
       (run, potentialCounterexamples)
+
     } else {
-      val run =
-        Run(runId.getAndIncrement, RunStatus("failure"), failureSpec, model, messages, provenance_orig)
+
+      val run = Run(runId.getAndIncrement, RunStatus("failure"), failureSpec, model, messages, provenance_orig)
+
       (run, Set.empty)
     }
   }
